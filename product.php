2a73c46f2ab82f60a36c7bac7661f1c281b0f5ab
swisyy <161968693+swisyy@users.noreply.github.com>
2024-03-10 10:45:30 -0500

    Add files via upload

---
+++ product.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+
+}
+
+input[type=BUTTON] {
+  width: 5%;
+  background-color:sandybrown;
+  color: white;
+  padding: 10px 20px;
+  margin: 20px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+input[type=BUTTON] {
+  width: 15%;
+  background-color:sandybrown;
+  color: white;
+  padding: 10px 20px;
+  margin: 20px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=BUTTON]:hover {
+  background-color: #sandybrown;
+}
+input[type=BUTTON]:hover {
+  background-color: #sandybrown;
+}
+
+div {
+  border-radius: 10px;
+  background-color: #f2f2f2;
+  padding: 50px;
+
+}
+label[for="price"]{
+    margin: 10px;
+    padding-right: 30px;
+
+}
+label[for="type"]{
+
+}
+
+</style>
+<body>
+<div class="form">
+  <form action="/product.php">
+    <label for="name"> Name</label>
+    <input type="text" id="name" name="product name" placeholder="product name..">
+
+    <label for="description">description</label>
+    <input type="text" id="description" name="description" placeholder="description..">
+    <label for="price">price</label></br>
+    <input type="text" id="price" name="price" placeholder="add a price.."></br>
+
+    <label for="type">Type</label>
+    <select id="type" name="type">
+      <option value="meals">meals</option>
+      <option value="dessert">dessert</option>
+      <option value="burgers">burgers</option>
+      <option value="drinks">drinks</option>
+      <input type="file" id="myFile" name="filename">
+      </br>
+    </select>
+    <input type="BUTTON" value="ADD">
+    <input type="BUTTON" value="RESET">
+
+  </form>
+</div>
+
+</body>
+</html>
+
+
